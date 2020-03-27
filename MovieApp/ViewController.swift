dc49b97dad2fb7f355e6460cfa8a0e6a790a32fb
Erkan CAN <erkancan@me.com>
2020-03-27 21:55:49 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Erkan CAN on 27.03.2020.
+//  Copyright © 2020 Erkan CAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
